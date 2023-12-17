446877a91221ebbcd510b846c737971242605bb7
Truong Van Khai <89502589+VanKhaiii@users.noreply.github.com>
2023-12-17 17:40:08 +0700

    hello

--- pages/02_🏘️_App.py
+++ pages/02_🏘️_App.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 import streamlit as st
 
 from src.phobert.model_predict import *
@@ -12,6 +13,10 @@ from src.biLSTM_CRF.load_model_bi import load_model_bilstm
 from src.biLSTM_CRF.utils import *
 from src.biLSTM_CRF.biLSTM_predict import predict_bilstm
 
+from src.viT5.load_model_vit5 import load_model_vit5
+from src.viT5.ultils import *
+from src.viT5.model_predict_vit5 import predict_vit5
+
 from src.ultils import rdrsegmenter
 
 st.set_page_config(
@@ -24,11 +29,14 @@ st.image(image=Image.open('D:\\Nam3\\NLP\\deloy_app\\1200x675.jpg'), caption='Na
 with st.spinner("Loading Model....(50%)"):
     model_bilstm = load_model_bilstm()
     
-with st.spinner("Loading Model....(100%)"):
+with st.spinner("Loading Model....(70%)"):
     model_phobert, tokenizer_phobert = load_model_phobert()
     
+with st.spinner("Loading Model....(100%)"):
+    model_vit5, tokenizer_vit5 = load_model_vit5()
+    
 
-model_name = st.sidebar.selectbox('Choose model?', ['PhoBert', 'biLSTM'])
+model_name = st.sidebar.selectbox('Choose model?', ['PhoBert', 'biLSTM', 'viT5'])
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
 # Driver
@@ -47,11 +55,20 @@ if st.button('Analyze'):
                  st.write(f"{' '.join(text[entity[1]: entity[2] + 1])} ==> {entity[0]}")
     if model_name == 'biLSTM':
         res, texts = predict_bilstm(sentences, model_bilstm, rdrsegmenter)
-        print("======" , texts)
         def show(pred, text):
             text = text.split()
             entites = get_entities(pred)
-            for entity in entites:
-                st.write(f"{' '.join(text[entity[1]: entity[2] + 1])} => {entity[0]}")
+            if len(entites) != 0:
+                for entity in entites:
+                    st.write(f"{' '.join(text[entity[1]: entity[2] + 1])} => {entity[0]}")
+            else:
+                st.write("No Tags Founds!")
         show(res[0], texts[0])
+    if model_name == 'viT5':
+        entities = predict_vit5(sentences, model_vit5, tokenizer_vit5, rdrsegmenter)
+        entity_list = [entity.strip() for entity in entities.split(';')]
+        st.write('*Ner Tags:*')
+        st.write('*Sentence:*', sentence)
+        for entity in entity_list:
+            st.write(entity)
                 
